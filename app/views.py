c17ef5d461ab72cc986969f11debec7ebea54715
Mike T <miketissenbaum@gmail.com>
2015-07-02 14:47:17 -0500

    all the goodness

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from flask import render_template
+from flask import request, jsonify
+from app import app
+
+# app.run(port=5001)
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+
+def index(): 
+	print "starting"
+	if request.method == 'POST':
+		# print(request.data)
+
+		location = {'mspace': 'Central Library'} 
+		user = {'nickname': request.args.get('name')}
+		print "works!"
+		print user['nickname']
+		return render_template('index.html',
+                    location = location,
+                    user = user) 
+	return render_template('index.html', 
+							location = "test", 
+							user = 'user')
---
+++ flask/lib/python2.7/fnmatch.py
@@ -0,0 +1,1 @@
+/Users/research/anaconda/lib/python2.7/fnmatch.py
