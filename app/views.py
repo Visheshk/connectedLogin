9e15b085a40aec1b59bfc9560acd254a9865c931
Mike T <miketissenbaum@gmail.com>
2015-07-06 14:36:56 -0500

    did a lot of stuff

--- app/views.py
+++ app/views.py
@@ -1,32 +1,47 @@
 from flask import render_template
-from flask import request, jsonify
+from flask import Flask, request, jsonify, session
 from app import app
 
 # app.run(port=5001)
 
-@app.route('/getSignIn', methods=['GET','POST'])
+checkCheck = False
 
+@app.route('/getSignIn', methods=['GET','POST'])
 def getSignIn():
-	list = [
-		{'newCheckin': 'true'}
-		]
+	global checkCheck
+	list = {'newCheckin': checkCheck}
+	checkCheck = False
 	return jsonify(list)
+
+@app.route('/activity', methods=['GET', 'POST'])
+def activity():
+	print session.get('var')
+	user = session.get('var')
+	location = {'mspace': 'Central Library'} 
+	return render_template('activity.html',
+							location = location,
+							user = user)
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
-
 def index(): 
+	global checkCheck
 	print "starting"
 	if request.method == 'POST':
 		# print(request.data)
-
+		checkCheck = True
 		location = {'mspace': 'Central Library'} 
 		user = {'nickname': request.args.get('name')}
-		print "works!"
-		print user['nickname']
+		session['var'] = request.args.get('name')
+		print user
+		print session['var']
+		print session.get('var')
 		return render_template('index.html',
                     location = location,
                     user = user) 
+
 	return render_template('index.html', 
 							location = "test", 
 							user = 'user')
+
+app.secret_key = 'you-will-never-guess'
