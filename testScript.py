a48eb6a8d32f28ca92618418abe3ec6d30a44d08
dennispr <dennispr@gmail.com>
2015-08-04 16:05:03 -0700

    added CORS support

--- testScript.py
+++ testScript.py
@@ -1,4 +1,5 @@
 import requests
-payload = {"cardID":"750047FB76BF"}
+#payload = {"cardID":"750047FB76BF"}
+payload = {"cardID":"1111"}
 print payload
 r = requests.post("http://127.0.0.1:5000/index", data=payload)
